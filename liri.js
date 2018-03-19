ebb2b8ff32ccb32fee5c47f44d4e295bcc441700
PRESTONPOWELL22 <preston@windowtorecovery.com>
2018-03-19 16:28:50 -0700

    set up filestructure but it liri can't find my api keys

---
+++ liri.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+var keys = require("./keys.js")
+
+var spotify = new Spotify(keys.spotify)
+var client = new Twitter(keys.twitter)
+
+var a = process.argv[3]
+
+var b = process.argv[4]
+
+console.log(keys)
