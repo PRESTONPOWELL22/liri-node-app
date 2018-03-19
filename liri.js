744dfdd37400a57b652ca7417b1ae27329f20476
PRESTONPOWELL22 <preston@windowtorecovery.com>
2018-03-19 16:55:19 -0700

    got keys working properly and added keys for spotify

--- liri.js
+++ liri.js
@@ -1,12 +1,13 @@
-require("dotenv").config();
+require("dotenv").config()
 
 var keys = require("./keys.js")
 
-var spotify = new Spotify(keys.spotify)
-var client = new Twitter(keys.twitter)
+// var spotify = new Spotify(keys.spotify)
+// var client = new Twitter(keys.twitter)
 
 var a = process.argv[3]
 
 var b = process.argv[4]
 
-console.log(keys)
+console.log(keys.spotify)
+console.log(keys.twitter)
